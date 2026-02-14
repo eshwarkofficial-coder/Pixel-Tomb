cb884ad69679ce6e41a179c1b2f615eb504206d4
Eshwar.k <eshwarkoficial@gmail.com>
2026-02-14 22:11:51 +0530

    new ui update

--- app.py
+++ app.py
@@ -189,5 +189,6 @@ def download_file(filename):
     return send_file(os.path.join(UPLOAD_FOLDER, filename), as_attachment=True)
 
 if __name__ == '__main__':
-    #app.run(debug=True)
-    app.run(host='0.0.0.0', port=int(os.environ.get("PORT", 5000)))
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
+
